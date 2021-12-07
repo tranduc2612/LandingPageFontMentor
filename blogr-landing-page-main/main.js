e5d63f8941db007ca4aaec54f5e5137b712c96b0
tranduc2612 <mintduc2612@gmail.com>
2021-12-07 10:04:01 +0700

    update

---
+++ blogr-landing-page-main/main.js
@@ -0,0 +1,8 @@
+const navToggles = document.querySelectorAll('.nav__toggle')
+const navtoggleIcon = document.querySelector('.nav__toggle-icon')
+
+for(var i=0;i<navToggles.length;i++){
+    navToggles[i].onclick = function(e){
+        console.log()
+    }
+}
